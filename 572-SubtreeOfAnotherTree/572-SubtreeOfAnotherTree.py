ff7754b34cd0401e80f2b144d403d9585a82258c
Justin Youngwook Kang <justinkangg@gmail.com>
2025-11-26 17:40:58 -0800

    Create 572-SubtreeOfAnotherTree

---
+++ 572-SubtreeOfAnotherTree/572-SubtreeOfAnotherTree.py
@@ -0,0 +1,21 @@
+# Last updated: 11/26/2025, 5:40:57 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def isSame(root, subRoot):
+            if not root and not subRoot:
+                return True
+            if not root or not subRoot:
+                return False
+            return root.val == subRoot.val and isSame(root.left, subRoot.left) and isSame(root.right, subRoot.right)
+
+        if not root:
+            return False
+        if isSame(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
